2b7e4cd860d8295bf45323b93d35103f5c8a5fe8
ella hain <ehain@bu.edu>
2025-11-13 23:16:36 -0500

    updtaing redirect logic

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -1,29 +1,6 @@
-"use client"
-import {redirect, useParams} from "next/navigation"
-import getCollection, {ALIAS_COLLECTION} from "@/db";
-import {AliasProps} from "@/types";
-import getAllAliases from "@/lib/getAllAliases";
-import {useState} from "react";
+import {redirect} from "next/navigation"
 
 
-
-
-export default function AliasPage() {
-    const alias = useParams()
-    const [value, setValue] = useState<AliasProps>({id: "", alias: "", url: ""} as AliasProps)
-
-    async function getURL() {
-        const aliasCollection = await getAllAliases()
-        for (let i = 0; i<aliasCollection.length; i++){
-            if (aliasCollection[i]===alias){
-                setValue(aliasCollection[i])
-            }
-        }
-    }
-
-    const info = getURL();
-
-    redirect(`${value.url}`)
-
-
+export default function AliasPage(url: string) {
+    redirect(`${url}`)
 }
